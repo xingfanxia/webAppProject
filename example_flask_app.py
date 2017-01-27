298216c7d7d3ae728d76ec80dfafbe4c366e6cf2
xxf1995 <xiax@carleton.edu>
2017-01-27 10:15:52 -0600

    some modification to send to thacker

--- example_flask_app.py
+++ example_flask_app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''
-    example_flask_app.py
-    Jeff Ondich, 22 April 2016
+	example_flask_app.py
+	Jeff Ondich, 22 April 2016
 	Modified by Eric Alexander, January 2017
 
-    A slightly more complicated Flask sample app than the
-    "hello world" app found at http://flask.pocoo.org/.
+	A slightly more complicated Flask sample app than the
+	"hello world" app found at http://flask.pocoo.org/.
 '''
 import flask
 import json
@@ -15,7 +15,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Citizen of CS257.'
+	return 'Hello, Citizen of CS257.'
 	
 @app.route('/fancier/')
 def itDoesHTML():
@@ -32,20 +32,22 @@ def itDoesHTML():
 
 @app.route('/authors/<author>')
 def get_author(author):
-    ''' What a dopey function! But it illustrates a Flask route with a parameter. '''
-    if author == 'Twain':
-        author_dictionary = {'last_name':'Twain', 'first_name':'Mark'}
-    elif author == 'Shakespeare':
+	''' What a dopey function! But it illustrates a Flask route with a parameter. '''
+	if author == 'Twain':
+		author_dictionary = {'last_name':'Twain', 'first_name':'Mark'}
+	elif author == 'Shakespeare':
 		author_dictionary = {'last_name':'Shakespeare', 'first_name':'William'}
+	elif author == 'Souzyu':
+		author_dictionary = {'last_name': 'Souzyu', 'first_name':'dChen'}
 	else:
-        author_dictionary = {'last_name':'McBozo', 'first_name':'Bozo'}
-    return json.dumps(author_dictionary)
+		author_dictionary = {'last_name':'McBozo', 'first_name':'Bozo'}
+	return json.dumps(author_dictionary)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
-        exit()
+	if len(sys.argv) != 3:
+		print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+		exit()
 
-    host = sys.argv[1]
-    port = sys.argv[2]
-    app.run(host=host, port=port)
+	host = sys.argv[1]
+	port = sys.argv[2]
+	app.run(host=host, port=port)
