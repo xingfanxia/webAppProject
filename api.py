76474dfd4991cd0232749ed484df43eb35582fa4
xxf1995 <xiax@carleton.edu>
2017-01-29 21:14:02 -0600

    test files

--- api.py
+++ api.py
@@ -1,1 +1,12 @@
 # API File containing all the HTML query parsing functions For WebApp
+
+@app.route('/books/<author_name>')
+def get_books(author_name):
+	""" Return a list of dictionaries written by the given author, each of which describes one book with keys 'title' and 'publication_year'.
+
+	Example: http://whatever.com/books/Austen
+	[{'title':'Pride and Prejudice', 'publication_year':1813},
+	 {'title':'Sense and Sensibility', 'publication_year':1813},
+	 {'title':'Emma', 'publication_year':1815}]
+	"""
+	return []
---
+++ testcases/test_get_author.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestGet_author(TestCase):
+	pass
