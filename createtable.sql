ccc85302a162f86950603e227677c3d9266e1968
xxf1995 <xiax@carleton.edu>
2017-02-03 04:42:41 -0600

    createtable.sql and remove headings from csv

---
+++ createtable.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (
+  surname text,
+  name text,
+  age real,
+  accel real,
+  agility real,
+  react real,
+  balance real,
+  stamina real,
+  strength real,
+  intercept real,
+  position real,
+  vision real
+);
