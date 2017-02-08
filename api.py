e20b3debe78339edaacc51c87aefa67727e576ba
wuk <wuk@carleton.edu>
2017-02-07 20:51:42 -0600

    7

--- api.py
+++ api.py
@@ -10,11 +10,11 @@ from flask import render_template, request
 # link to database
 database = getpass.getuser()
 user = getpass.getuser()
-password = getpass.getpass('Enter PostgreSQL password for user {}: '.format(user))
+#password = getpass.getpass('Enter PostgreSQL password for user {}: '.format(user))
 
 # Login to the database
 try:
-	connection = psycopg2.connect(database=database, user=user, password=password)
+	connection = psycopg2.connect(database=database, user=user, password='moon329tiger')
 except Exception as e:
 	print(e)
 	exit() 
