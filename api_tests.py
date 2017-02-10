f9fb54ee7c1632b5fcfcea79d6737252c56308f2
xxf1995 <xiax@carleton.edu>
2017-02-09 21:39:52 -0600

    how to avoid 403

--- api_tests.py
+++ api_tests.py
@@ -15,7 +15,7 @@ class apiTester(TestCase):
 		self.assertTrue(api.getAllAttributes("Cristiano Ronaldo") != [])
 
 	def testCompareDifference(self):
-		self.assertTrue(api.compareDifference("L. Messi","Pepe") != [])
+		self.assertTrue(api.compareDifference("Messi","Pepe") != [])
 	def testSimilarPlayer(self):
 		self.assertTrue(api.similarPlayer("Cristiano Ronaldo") != [])
 	def testCalculateCos(self):
