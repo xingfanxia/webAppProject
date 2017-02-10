415cfb323169c977cfa44171bba57a12c7846797
xxf1995 <xiax@carleton.edu>
2017-02-09 21:55:53 -0600

    modify expectaation

--- api_tests.py
+++ api_tests.py
@@ -10,8 +10,8 @@ class apiTester(TestCase):
 	    self.assertTrue(api.getAllPlayer() != [])
 
 	def testGetAllAttributes(self):
-		print(api.getAllAttributes("Cristiano Ronaldo"))
-		self.assertTrue(api.getAllAttributes("Cristiano Ronaldo") != [])
+		result = ('Ronaldo', 'Cristiano Ronaldo', 31.0, 91.0, 90.0, 96.0, 63.0, 92.0, 80.0, 29.0, 94.0, 85.0)
+		self.assertTrue(api.getAllAttributes("Cristiano Ronaldo") == result)
 
 	def testCompareDifference(self):
 		self.assertTrue(api.compareDifference("Cristiano Ronaldo","Pepe") != [])
