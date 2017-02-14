d3265e9e500d807757add77fb946e94512c07e2a
xxf1995 <xiax@carleton.edu>
2017-02-14 15:22:26 -0600

    callback

---
+++ README.md
@@ -0,0 +1,22 @@
+## Fifa Database WebApp Project
+
+### `@author Xingfan Xia & Kaixing Wu`
+
+### Description
+
+This project is dedicated to create a webapp for Fifa fans to search for players they are interested in.
+
+### Features
+
+- Search for a player and show his stats
+- Compare two players' stats
+- Find similar players for player of interest
+
+### Citation of Database
+
+### One To-Do Feature
+
+- Advance Search
+
+### Link to script?
+
---
+++ website/static/js/parseJson.js
@@ -0,0 +1,31 @@
+function onGetPlayerStats() {
+	event.preventDefault();
+    var url = '/playerStats/';
+    xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open('get', url);
+    
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+            playerStatsCallback(JSON.parse(xmlHttpRequest.response));
+        }
+    }
+    
+    xmlHttpRequest.send(null)
+}
+
+function playerStatsCallback(jsonResponse) {
+    var statsList = jsonResponse['results'];
+    var stringdisplay = "";
+    for (var i = 0; i < statsList.length; i++) {
+        stringdisplay += statsList[i];
+    }
+    // var fruitStr = ''
+    // fruitStr += '<table><tr><th>name</th><th>rating</th></tr>'
+    // for (var i = 0; i < fruitList.length; i++) {
+    //     fruitStr += '<tr><td>' + fruitList[i]['name'] + '</td><td>' + fruitList[i]['rating'] + '</td></tr>'
+    // }
+    // fruitStr += '</table>'
+    
+    var statsDiv = document.getElementById('displayResult');
+    fruitDiv.innerHTML = stringdisplay;
+}
