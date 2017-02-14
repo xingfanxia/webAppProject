7acf8997eeb73e36d770cbe30f8aa8cf30d492e8
xxf1995 <xiax@carleton.edu>
2017-02-14 16:16:00 -0600

    maybe

--- website/static/js/parseJson.js
+++ website/static/js/parseJson.js
@@ -1,10 +1,60 @@
+var randomScalingFactor = function() {
+    return Math.round(Math.random() * 100);
+};
+var randomColorFactor = function() {
+    return Math.round(Math.random() * 255);
+};
+var randomColor = function(opacity) {
+    return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+};
+
+var config = {
+    type: 'radar',
+    data: {
+        labels: ["Accel", "Agility", "React", 
+    "Balance"," Stamina", "Strength", 
+    "Intercept", "Position", "Vision"],
+        datasets: [{
+            label: "Player Stats",
+            // borderColor: 'rgb(30,144,255)',
+            backgroundColor: "rgba(0,191,255,0.5)",
+            pointBackgroundColor: "rgba(220,220,220,1)",
+            pointHoverBackgroundColor: "#fff",
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }]
+    },
+    options: {
+        title:{
+            display:true,
+            text:"Demo Chart"
+        },
+        elements: {
+            line: {
+                tension: 0.0,
+            }
+        },
+        scale: {
+            beginAtZero: true,
+            reverse: false
+        }
+    }
+};
+
+window.onload = function() {
+    window.myRadar = new Chart(document.getElementById("canvas"), config);
+};
+
+function updateGraph(listATTR) {
+	config[data][datasets][data] = listATTR;
+}
+
+
 function onGetPlayerStats() {
 	event.preventDefault();
 	var srchTerm = document.getElementById('srch-term-player').value;
     var url = '/playerStats/'+srchTerm;
     xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.open('get', url);
-    alert("parsing");
     xmlHttpRequest.onreadystatechange = function() {
         if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
             playerStatsCallback(JSON.parse(xmlHttpRequest.response));
@@ -18,7 +68,7 @@ function playerStatsCallback(jsonResponse) {
     var statsList = jsonResponse['results'];
     var stringdisplay = "";
     for (var i = 0; i < statsList.length; i++) {
-        stringdisplay += statsList[i];
+        stringdisplay += statsList[i] + ",";
     }
     // var fruitStr = ''
     // fruitStr += '<table><tr><th>name</th><th>rating</th></tr>'
@@ -26,7 +76,8 @@ function playerStatsCallback(jsonResponse) {
     //     fruitStr += '<tr><td>' + fruitList[i]['name'] + '</td><td>' + fruitList[i]['rating'] + '</td></tr>'
     // }
     // fruitStr += '</table>'
-    alert("callback received");
     var statsDiv = document.getElementById('displayResult');
     statsDiv.innerHTML = stringdisplay;
+    updateGraph(statsList);
 }
+
